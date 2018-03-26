cedc6c29cec14ee4bd2f4ca911e6e6d5b98cc061
jennifer_lin <jennifer_lin@mail.chungyo.net>
2018-03-26 18:01:55 +0800

    time function OK

--- src/js/main.js
+++ src/js/main.js
@@ -9,17 +9,19 @@ function orderList_open() {
 //clock
 document.body.onload = function startTime() {   //body.onload的時候執行此function
   var today = new Date();                       //取得本地時間
+  today.setTime(today.getTime() - 7 * 60 * 60 * 1000);
   var y = today.getFullYear();                  //取得年份
-  var M = today.getMonth();                     //取得月份
-  var d = today.getDay();                       //取得日期
+  var M = today.getMonth() + 1;                     //取得月份
+  var d = today.getDate();                       //取得日期
   var h = today.getHours();                     //取得小時
   var m = today.getMinutes();                   //取得分鐘
-  var s = today.getSeconds();                   //取得秒數
+  var s = today.getSeconds();
+  h = checkTime(h);                    //取得秒數
   m = checkTime(m);                             //將m代入function checkTime
   s = checkTime(s);
   document.getElementById('sysTime').innerHTML =
     y + "/" + M + "/" + d + " " + h + ":" + m + ":" + s;  //顯示格式
-  var t = setTimeout(startTime, 500);       //宣告t 每五百毫秒重新取得本地時間
+  var t = setTimeout(startTime, 1000);       //宣告t 每五百毫秒重新取得本地時間
 }
 function checkTime(i) {                      //將個位數的值前面加上“0"
   if (i < 10) {
